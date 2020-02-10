9b6dded55d23d83750eac55bd2fe889a91388f1d
frankthoeny <frankthoeny@yahoo.com>
2020-02-10 08:37:52 -0800

    Bootstrap carousel

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  itemsPerSlide = 5;
+  startFromIndex = 0;
+  singleSlideOffset = false;
+  interval = false; 
+  slides = [
+    {image: '/assets/images/nature/1.jpg'},
+    {image: '/assets/images/nature/2.jpg'},
+    {image: '/assets/images/nature/3.jpg'},
+    {image: '/assets/images/nature/4.jpg'},
+    {image: '/assets/images/nature/5.jpg'},
+    {image: '/assets/images/nature/6.jpg'},
+    {image: '/assets/images/nature/7.jpg'},
+    {image: '/assets/images/nature/8.jpg'},
+    {image: '/assets/images/nature/1.jpg'},
+    {image: '/assets/images/nature/2.jpg'}
+  ];
+
+}
